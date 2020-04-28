045ab06ad8bfc3f116b1c2c8ed2d8685656e99df
Adam Hartley <ahartley@benevity.com>
2020-04-28 15:40:42 -0600

    Modifications include:      * Adding new resuable TextInput.vue component, which can be utilized in parent components requiring a text area. An emit event has been included in the TextInput.vue which reacts to keyboard events.

---
+++ src/components/card/TextInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="form-group">
+        <h4>Edit Text:</h4>
+        <textarea class="form-control" cols="50" rows="4"
+                  placeholder="Add text here!"
+                  v-model="textBoxInput"
+                  @keyup="textChanged">
+        </textarea>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        textBoxInput: ""
+      }
+    },
+    methods: {
+      /*
+       * textChanged is called on each keyboard event, and emits the textBoxInput to the parent.
+       * The custom event displayTextChanged can be added to parent components which can listen for
+       * event changes.
+       */
+      textChanged: function () {
+        this.$emit('displayTextChanged', this.textBoxInput)
+      }
+    }
+  }
+</script>
