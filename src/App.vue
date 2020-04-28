a01b7bc160e5ed09abc3e77133f5fedfa5ae9c8d
Adam Hartley <ahartley@benevity.com>
2020-04-28 15:03:43 -0600

    Modifications include:      * Adding new CardBack.vue, CardInsideLeft.vue, and CardInsideRight.vue components      * Added click events to Header.vue nav links to set pageSelected variable      * Added component tag to App.vue to support dynamic components. When a nav link is clicked on, using the v-bind is directive to switch components dynamically.      * Default component set to cardFront in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
-        <nav-header></nav-header>
-        <card-front></card-front>
+        <nav-header @pageWasChanged="currentPage = $event"></nav-header>
+        <component :is="currentPage"></component> <!-- component listens for the $emit event and updates the currentPage data -->
       </div>
     </div>
   </div>
@@ -12,11 +12,22 @@
 <script>
   import Header from './components/Header.vue'
   import CardFront from './components/card/CardFront.vue'
+  import CardInsideLeft from './components/card/CardInsideLeft.vue'
+  import CardInsideRight from './components/card/CardInsideRight.vue'
+  import CardBack from './components/card/CardBack.vue'
 
   export default {
+    data: function () {
+      return {
+        currentPage: 'cardFront' // displays the front of the card by default
+      }
+    },
     components: {
       navHeader: Header,
-      cardFront: CardFront
+      cardFront: CardFront,
+      cardInsideLeft: CardInsideLeft,
+      cardInsideRight: CardInsideRight,
+      cardBack: CardBack
     }
   }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
-    <h1 class="text-center">Created Cards</h1>
+    <h1 class="text-center">Creative Cards</h1>
     <ul class="nav justify-content-center">
-      <li class="nav-item"><a class="nav-link">Front</a></li>
-      <li class="nav-item"><a class="nav-link">Inside Left</a></li>
-      <li class="nav-item"><a class="nav-link">Inside Right</a></li>
-      <li class="nav-item"><a class="nav-link">Inside Back</a></li>
+      <li class="nav-item"><a class="nav-link" @click="pageSelected='cardFront'">Front</a></li>
+      <li class="nav-item"><a class="nav-link" @click="pageSelected='cardInsideLeft'">Inside Left</a></li>
+      <li class="nav-item"><a class="nav-link" @click="pageSelected='cardInsideRight'">Inside Right</a></li>
+      <li class="nav-item"><a class="nav-link" @click="pageSelected='cardBack'">Back</a></li>
     </ul>
   </div>
 </template>
 
 <script>
-
+  export default {
+    data: function () {
+      return {
+        pageSelected: ''
+      }
+    },
+    /*
+     * An object where keys are expressions to watch and values are the corresponding callbacks
+     */
+    watch: {
+      pageSelected: function () {
+        // emitting custom event to parent (App.vue)
+        this.$emit('pageWasChanged', this.pageSelected)
+      }
+    }
+  }
 </script>
 
 <style>
